0dcac2608b144f93b7729dbfe24608e72205a26e
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-10-23 00:31:18 +0800

    Update RecyclerViewWithContextMenu.kt

--- app/src/main/java/top/abr/androidexp6/RecyclerViewWithContextMenu.kt
+++ app/src/main/java/top/abr/androidexp6/RecyclerViewWithContextMenu.kt
@@ -1,5 +1,38 @@
 package top.abr.androidexp6
 
-class RecyclerViewWithContextMenu {
-	
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ContextMenu
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewWithContextMenu : RecyclerView {
+	class RecyclerViewContextInfo : ContextMenu.ContextMenuInfo {
+		var Position = -1
+	}
+
+	private val ContextInfo: RecyclerViewContextInfo = RecyclerViewContextInfo()
+
+	constructor(Ctx: Context) : super(Ctx)
+
+	constructor(Ctx: Context, Attrs: AttributeSet?) : super(Ctx, Attrs)
+
+	constructor(Ctx: Context, Attrs: AttributeSet?, DefStyleAttr: Int) : super(Ctx, Attrs, DefStyleAttr)
+
+	private fun GetPositionByChild(OriginalView: View) {
+		if (layoutManager != null) {
+			val Pos = layoutManager!!.getPosition(OriginalView)
+			ContextInfo.Position = Pos
+		}
+	}
+
+	override fun showContextMenuForChild(OriginalView: View?): Boolean {
+		GetPositionByChild(OriginalView!!)
+		return super.showContextMenuForChild(OriginalView)
+	}
+
+	override fun showContextMenuForChild(OriginalView: View?, x: Float, y: Float): Boolean {
+		GetPositionByChild(OriginalView!!)
+		return super.showContextMenuForChild(OriginalView, x, y)
+	}
 }
