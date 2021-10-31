8316da878a8f7a8a8bad2662e547d0a1f6f5fd29
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-10-31 12:43:58 +0800

    Update BookListMainActivity.kt

--- app/src/main/java/top/abr/androidexp6/BookListMainActivity.kt
+++ app/src/main/java/top/abr/androidexp6/BookListMainActivity.kt
@@ -1,13 +1,29 @@
 package top.abr.androidexp6
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class BookListMainActivity : AppCompatActivity() {
+	open inner class EditBookInformation : ActivityResultContract<Book, Book>() {
+		override fun createIntent(AppContext: Context, BookForEdit: Book) =
+			Intent(this@BookListMainActivity, EditBookActivity::class.java).apply {
+				putExtra("Book.Title", BookForEdit.Title)
+			}
+
+		override fun parseResult(ResultCode: Int, IntentWithResult: Intent?): Book? {
+			if (ResultCode != Activity.RESULT_OK) return null
+			return Book(Title = IntentWithResult?.getStringExtra("Book.Title")!!)
+		}
+	}
+
 	val BookList = ArrayList<Book>(
 		arrayListOf(
 			Book(R.drawable.book_2, "软件项目管理案例教程（第4版）"),
@@ -28,9 +44,14 @@ class BookListMainActivity : AppCompatActivity() {
 	}
 
 	override fun onContextItemSelected(MItem: MenuItem): Boolean {
+		val MainBooksAdapter = BookListView.adapter as BooksAdapter
 		when (MItem.itemId) {
+			0 -> {
+				val EditBookActivityLauncher = registerForActivityResult(EditBookInformation()) {
+					BookList[MainBooksAdapter.MPosition].Title = it.Title
+				}.launch(MainBooksAdapter.BookList[MainBooksAdapter.MPosition])
+			}
 			1 -> {
-				val MainBooksAdapter = BookListView.adapter as BooksAdapter
 				MainBooksAdapter.DeleteBookItem(MainBooksAdapter.MPosition)
 			}
 		}
