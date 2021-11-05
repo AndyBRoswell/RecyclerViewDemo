2d25cb2d7ae66b26dbe3970af4467fff77f04e6c
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-05 14:15:40 +0800

    /

--- app/src/main/java/top/abr/androidexp6/Book.kt
+++ app/src/main/java/top/abr/androidexp6/Book.kt
@@ -14,21 +14,5 @@ class Book constructor(@DrawableRes var CoverResourceID: Int = 0, var Title: Str
 			Hasher.update("top.abr.androidexp6.Book-2021-11-05".toByteArray())
 			serialVersionUID = ByteBuffer.wrap(Hasher.digest()).long
 		}
-
-		fun ReadBookList(SerializedFile: String, A: BooksAdapter) {
-			val BookListIStream = ObjectInputStream(FileInputStream(SerializedFile))
-			A.BookList.clear()
-			var B: Book
-			var HasNext = true
-			while (HasNext) {
-				B = BookListIStream.readObject() as Book
-				if (B != null) A.BookList.add(B)
-				else HasNext = false
-			}
-		}
-
-		fun SaveBookList(SerializedFile: String) {
-			val BookListOStream = ObjectOutputStream(FileOutputStream(SerializedFile))
-		}
 	}
 }
--- app/src/main/java/top/abr/androidexp6/StorageAccessor.kt
+++ app/src/main/java/top/abr/androidexp6/StorageAccessor.kt
@@ -1,5 +1,33 @@
 package top.abr.androidexp6
 
-open class StorageAccessor {
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
+open class StorageAccessor {
+	companion object {
+		fun ReadBookList(A: BooksAdapter, SerializedFile: String = "BookList.txt") {
+			val BookListIStream = ObjectInputStream(FileInputStream(SerializedFile))
+			A.BookList.clear()
+			var B: Book
+			var HasNext = true
+			while (HasNext) {
+				B = BookListIStream.readObject() as Book
+				if (B != null) A.BookList.add(B)
+				else HasNext = false
+			}
+			BookListIStream.close()
+			A.notifyItemChanged(0)
+		}
+
+		fun SaveBookList(A: BooksAdapter, SerializedFile: String = "BookList.txt") {
+			val BookListOStream = ObjectOutputStream(FileOutputStream(SerializedFile))
+			for (B in A.BookList) {
+				BookListOStream.writeObject(B)
+			}
+			BookListOStream.writeObject(null)
+			BookListOStream.close()
+		}
+	}
 }
