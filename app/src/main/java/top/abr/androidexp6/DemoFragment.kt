cb1e20722e1cb07100b9d3082e08c26a4b4f55de
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-16 22:59:20 +0800

    /

--- app/src/main/java/top/abr/androidexp6/DemoFragment.kt
+++ app/src/main/java/top/abr/androidexp6/DemoFragment.kt
@@ -13,8 +13,7 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DemoFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Use the [DemoFragment.newInstance] factory method to create an instance of this fragment.
  */
 class DemoFragment : Fragment() {
 	// TODO: Rename and change types of parameters
@@ -29,18 +28,14 @@ class DemoFragment : Fragment() {
 		}
 	}
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_demo, container, false)
 	}
 
 	companion object {
 		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
+		 * Use this factory method to create a new instance of this fragment using the provided parameters.
 		 *
 		 * @param param1 Parameter 1.
 		 * @param param2 Parameter 2.
