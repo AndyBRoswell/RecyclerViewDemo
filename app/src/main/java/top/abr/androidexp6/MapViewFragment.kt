dd5488c368d507bc732fa79b79352dc5035d9251
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-10 10:18:35 +0800

    FIXED A CRITICAL BUG at the ctor call of LatLng

--- app/src/main/java/top/abr/androidexp6/MapViewFragment.kt
+++ app/src/main/java/top/abr/androidexp6/MapViewFragment.kt
@@ -21,76 +21,76 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MapViewFragment.newInstance] factory method to create an instance of this fragment.
  */
 class MapViewFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-	lateinit var FragmentMapView: FragmentMapViewBinding
-	lateinit var BaiduMapView: MapView
-	lateinit var MBaiduMap: BaiduMap
-	val DefaultInitialPosition = LatLng(113.541112, 22.255925)
-	lateinit var MapMarker: Marker
+    lateinit var FragmentMapView: FragmentMapViewBinding
+    lateinit var BaiduMapView: MapView
+    lateinit var MBaiduMap: BaiduMap
+    val DefaultInitialPosition = LatLng(22.255925, 113.541112)      // LATITUDE first, and then LONGITUDE
+    lateinit var MapMarker: Marker
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
 
-		SDKInitializer.initialize(this.requireActivity().applicationContext)
-		SDKInitializer.setCoordType(CoordType.BD09LL)
-	}
+        SDKInitializer.initialize(this.requireActivity().applicationContext)
+        SDKInitializer.setCoordType(CoordType.BD09LL)
+    }
 
-	override fun onCreateView(LI: LayoutInflater, Container: ViewGroup?, SavedInstanceState: Bundle?): View {
-		// Inflate the layout for this fragment
-		FragmentMapView = FragmentMapViewBinding.inflate(LI, Container, false)
-		return FragmentMapView.root
-	}
+    override fun onCreateView(LI: LayoutInflater, Container: ViewGroup?, SavedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        FragmentMapView = FragmentMapViewBinding.inflate(LI, Container, false)
+        return FragmentMapView.root
+    }
 
-	override fun onViewCreated(V: View, SavedInstanceState: Bundle?) {
-		BaiduMapView = FragmentMapView.BaiduMapView
-		MBaiduMap = BaiduMapView.map
-		MBaiduMap.mapType = BaiduMap.MAP_TYPE_NORMAL
-		BaiduMapView.logoPosition = LogoPosition.logoPostionCenterTop
-		val MMapStatus = MapStatus.Builder().target(DefaultInitialPosition).build()
-		val MMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(MMapStatus)
-		MBaiduMap.setMapStatus(MMapStatusUpdate)
-		val MarkerBitmap = BitmapDescriptorFactory.fromResource(R.drawable.marker)
-		val MarkerOption = MarkerOptions().position(DefaultInitialPosition).icon(MarkerBitmap)
-		MapMarker = MBaiduMap.addOverlay(MarkerOption) as Marker
-	}
+    override fun onViewCreated(V: View, SavedInstanceState: Bundle?) {
+        BaiduMapView = FragmentMapView.BaiduMapView
+        MBaiduMap = BaiduMapView.map
+        MBaiduMap.mapType = BaiduMap.MAP_TYPE_NORMAL
+        BaiduMapView.logoPosition = LogoPosition.logoPostionCenterTop
+        val MMapStatus = MapStatus.Builder().target(DefaultInitialPosition).build()
+        val MMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(MMapStatus)
+        MBaiduMap.setMapStatus(MMapStatusUpdate)
+        val MarkerBitmap = BitmapDescriptorFactory.fromResource(R.drawable.marker)
+        val MarkerOption = MarkerOptions().position(DefaultInitialPosition).icon(MarkerBitmap)
+        MapMarker = MBaiduMap.addOverlay(MarkerOption) as Marker
+    }
 
-	override fun onResume() {
-		super.onResume()
-		BaiduMapView.onResume()
-	}
+    override fun onResume() {
+        super.onResume()
+        BaiduMapView.onResume()
+    }
 
-	override fun onPause() {
-		super.onPause()
-		BaiduMapView.onPause()
-	}
+    override fun onPause() {
+        super.onPause()
+        BaiduMapView.onPause()
+    }
 
-	override fun onDestroy() {
-		super.onDestroy()
-		BaiduMapView.onDestroy()
-	}
+    override fun onDestroy() {
+        super.onDestroy()
+        BaiduMapView.onDestroy()
+    }
 
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment MapViewFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic fun newInstance(param1: String, param2: String) =
-			MapViewFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
-	}
+    companion object {
+        /**
+         * Use this factory method to create a new instance of this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MapViewFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+            MapViewFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
