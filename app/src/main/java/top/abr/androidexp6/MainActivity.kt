af370d11d0e70938a5fd765f5310865c34e2ae83
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-13 10:22:02 +0800

    reindent with 4 spaces

--- app/src/main/java/top/abr/androidexp6/MainActivity.kt
+++ app/src/main/java/top/abr/androidexp6/MainActivity.kt
@@ -11,37 +11,37 @@ import com.google.android.material.tabs.TabLayoutMediator
 import top.abr.androidexp6.databinding.ActivityMainBinding
 
 open class PageAdapter(FAWithViewPager2: FragmentActivity, val TabCount: Int) : FragmentStateAdapter(FAWithViewPager2) {
-	override fun getItemCount(): Int = TabCount
+    override fun getItemCount(): Int = TabCount
 
-	override fun createFragment(Pos: Int): Fragment {
-		return when (Pos) {
-			0 -> BookListFragment()
-			1 -> MessageFragment()
-			2 -> MapViewFragment()
-			else -> DemoFragment()
-		}
-	}
+    override fun createFragment(Pos: Int): Fragment {
+        return when (Pos) {
+            0 -> BookListFragment()
+            1 -> MessageFragment()
+            2 -> MapViewFragment()
+            else -> DemoFragment()
+        }
+    }
 }
 
 class MainActivity : AppCompatActivity() {
-	val HOME_TAB_COUNT = 3
-	val TabTitle = arrayOf("图书", "新闻", "卖家")
+    val HOME_TAB_COUNT = 3
+    val TabTitle = arrayOf("图书", "新闻", "卖家")
 
-	lateinit var ActivityMain: ActivityMainBinding
-	lateinit var TabSelector: TabLayout
-	lateinit var TabDisplayer: ViewPager2
+    lateinit var ActivityMain: ActivityMainBinding
+    lateinit var TabSelector: TabLayout
+    lateinit var TabDisplayer: ViewPager2
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		ActivityMain = ActivityMainBinding.inflate(layoutInflater)
-		TabSelector = ActivityMain.TabSelector
-		TabDisplayer = ActivityMain.TabDisplayer
-		setContentView(ActivityMain.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityMain = ActivityMainBinding.inflate(layoutInflater)
+        TabSelector = ActivityMain.TabSelector
+        TabDisplayer = ActivityMain.TabDisplayer
+        setContentView(ActivityMain.root)
 
-		TabDisplayer.adapter = PageAdapter(this, HOME_TAB_COUNT)
+        TabDisplayer.adapter = PageAdapter(this, HOME_TAB_COUNT)
 
-		TabLayoutMediator(TabSelector, TabDisplayer) { CurrentTab, CurrentPos ->
-			CurrentTab.text = TabTitle[CurrentPos]
-		}.attach()
-	}
+        TabLayoutMediator(TabSelector, TabDisplayer) { CurrentTab, CurrentPos ->
+            CurrentTab.text = TabTitle[CurrentPos]
+        }.attach()
+    }
 }
--- app/src/main/java/top/abr/androidexp6/MessageFragment.kt
+++ app/src/main/java/top/abr/androidexp6/MessageFragment.kt
@@ -20,57 +20,57 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MessageFragment.newInstance] factory method to create an instance of this fragment.
  */
 class MessageFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-	val DefaultURL = "https://news.sina.cn"
+    val DefaultURL = "https://news.sina.cn"
 
-	lateinit var FragmentMessage: FragmentMessageBinding
-	lateinit var WebpageView: WebView
+    lateinit var FragmentMessage: FragmentMessageBinding
+    lateinit var WebpageView: WebView
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-	override fun onCreateView(LI: LayoutInflater, Container: ViewGroup?, SavedInstanceState: Bundle?): View {
-		// Inflate the layout for this fragment
-		FragmentMessage = FragmentMessageBinding.inflate(LI, Container, false)
-		return FragmentMessage.root
-	}
+    override fun onCreateView(LI: LayoutInflater, Container: ViewGroup?, SavedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        FragmentMessage = FragmentMessageBinding.inflate(LI, Container, false)
+        return FragmentMessage.root
+    }
 
-	override fun onViewCreated(V: View, SavedInstanceState: Bundle?) {
-		super.onViewCreated(V, SavedInstanceState)
+    override fun onViewCreated(V: View, SavedInstanceState: Bundle?) {
+        super.onViewCreated(V, SavedInstanceState)
 
-		WebpageView = FragmentMessage.WebpageView
-		WebpageView.webViewClient = object : WebViewClient() {
-			override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-				return false
-			}
-		}
-		WebpageView.settings.javaScriptEnabled = true
-		WebpageView.loadUrl(DefaultURL)
-	}
+        WebpageView = FragmentMessage.WebpageView
+        WebpageView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+        WebpageView.settings.javaScriptEnabled = true
+        WebpageView.loadUrl(DefaultURL)
+    }
 
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment MessageFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic fun newInstance(param1: String, param2: String) =
-			MessageFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
-	}
+    companion object {
+        /**
+         * Use this factory method to create a new instance of this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MessageFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+            MessageFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
 }
