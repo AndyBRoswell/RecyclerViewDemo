239a269a19eb2766188e88a885db0d02662e6ec2
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-13 14:16:49 +0800

    Update ShopLoader.kt

--- app/src/main/java/top/abr/androidexp6/ShopLoader.kt
+++ app/src/main/java/top/abr/androidexp6/ShopLoader.kt
@@ -1,18 +1,37 @@
 package top.abr.androidexp6
 
+import org.json.JSONArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
+import kotlin.math.*
+
+open class Shop(Name: String, Longitude: Double, Latitude: Double, Memo: String) {
+    var Name: String = ""
+    var Longitude: Double = 0.0
+    var Latitude: Double = 0.0
+    var Memo: String = ""
+
+    init {
+        if ((abs(Longitude) > 180.0) or (abs(Latitude) > 90.0)) throw IllegalArgumentException()
+    }
+}
 
 open class ShopLoader {
     companion object {
-        fun download() {
+        fun download(): String {
             val ShopListURL = URL("http://file.nidama.net/class/mobile_develop/data/bookstore.json")
             val URLContentReader = BufferedReader(InputStreamReader(ShopListURL.openStream()))
-            
+            val URLContentBuilder = StringBuilder()
+            while (true) {
+                val CurrentLine = URLContentReader.readLine()
+                if (CurrentLine != null) URLContentBuilder.append(CurrentLine)
+                else break
+            }
+            return URLContentBuilder.toString()
         }
 
-        fun parsonJson() {
+        fun parsonJson(JSONString: String): ArrayList<Shop> {
 
         }
     }
