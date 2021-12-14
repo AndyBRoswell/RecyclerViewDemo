17603a8f1ca5c0a0e01931cd82327391626b01e6
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-14 18:08:23 +0800

    adding game fragment

---
+++ app/src/main/java/top/abr/androidexp6/GameView.kt
@@ -0,0 +1,29 @@
+package top.abr.androidexp6
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+open class GameView : SurfaceView, SurfaceHolder.Callback {
+    constructor(Context: Context) : super(Context)
+    constructor(Context: Context, Attrs: AttributeSet) : super(Context, Attrs)
+    constructor(Context: Context, Attrs: AttributeSet, DefStyleAttr: Int) : super(Context, Attrs, DefStyleAttr)
+    constructor(Context: Context, Attrs: AttributeSet, DefStyleAttr: Int, DefStyleRes: Int) : super(Context, Attrs, DefStyleAttr, DefStyleRes)
+
+    init {
+        holder.addCallback(this)
+    }
+
+    override fun surfaceCreated(Holder: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceChanged(Holder: SurfaceHolder, Format: Int, Width: Int, Height: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(Holder: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+}
