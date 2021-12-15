7b7647be04eb5609fa0e68af4db41188d842a569
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-15 12:16:14 +0800

    Update SquareSprite.kt

--- app/src/main/java/top/abr/androidexp6/SquareSprite.kt
+++ app/src/main/java/top/abr/androidexp6/SquareSprite.kt
@@ -5,17 +5,20 @@ import android.graphics.Canvas
 typealias GraphicCoordCpntType = Float
 
 class SquareSprite {
-    var Coord: Coord2D<GraphicCoordCpntType>
+    var CenterCoord: Coord2D<GraphicCoordCpntType>
+    var MaxCoord: Coord2D<GraphicCoordCpntType>
     var EdgeLength: GraphicCoordCpntType
 
-    constructor(Coord: Coord2D<GraphicCoordCpntType>, EdgeLength: GraphicCoordCpntType) {
-        this.Coord = Coord
+    constructor(Coord: Coord2D<GraphicCoordCpntType>, EdgeLength: GraphicCoordCpntType, MaxCoord: Coord2D<GraphicCoordCpntType>) {
+        this.CenterCoord = Coord
         this.EdgeLength = EdgeLength
+        this.MaxCoord = MaxCoord
     }
 
-    constructor(x: GraphicCoordCpntType, y: GraphicCoordCpntType, EdgeLength: GraphicCoordCpntType) {
-        Coord = Coord2D(x, y)
+    constructor(x: GraphicCoordCpntType, y: GraphicCoordCpntType, EdgeLength: GraphicCoordCpntType, xmax: GraphicCoordCpntType, ymax: GraphicCoordCpntType) {
+        CenterCoord = Coord2D(x, y)
         this.EdgeLength = EdgeLength
+        MaxCoord = Coord2D(xmax, ymax)
     }
 
     fun Shot(TouchCoord: Coord2D<GraphicCoordCpntType>): Boolean {
@@ -27,7 +30,22 @@ class SquareSprite {
     }
 
     fun Move(DeltaC: Coord2D<GraphicCoordCpntType>) {
+        CenterCoord.x += DeltaC.x
+        CenterCoord.y += DeltaC.y
+        CorrectOutOfBound()
+    }
 
+    fun Move(DeltaX: GraphicCoordCpntType, DeltaY: GraphicCoordCpntType) {
+        CenterCoord.x += DeltaX
+        CenterCoord.y += DeltaY
+        CorrectOutOfBound()
+    }
+
+    private fun CorrectOutOfBound() {
+        if (CenterCoord.x < 0) CenterCoord.x += MaxCoord.x
+        if (CenterCoord.y < 0) CenterCoord.y += MaxCoord.y
+        if (CenterCoord.x > MaxCoord.x) CenterCoord.x -= MaxCoord.x
+        if (CenterCoord.y > MaxCoord.y) CenterCoord.y -= MaxCoord.y
     }
 
     fun DrawAt(Canvas: Canvas) {
